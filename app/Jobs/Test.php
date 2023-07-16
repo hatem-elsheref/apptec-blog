a1fbec5a45ffced69e3385ca854505833b85c06b
hatem-dev <hatem.mohamed@creatifyteam.com>
2023-07-16 14:11:07 +0300

    demo works fine

---
+++ app/Jobs/Test.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class Test implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $tries = 3;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(private $url, private $file_path, private $start, private $size){}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        if (is_null($this->url)) $this->fail("Please Check Url First , Then Try Again");
+
+        $fileHandler = fopen($this->file_path, 'rb');
+        fseek($fileHandler, $this->start);
+        $chunk = fread($fileHandler, $this->size);
+
+//        $chunk = file_get_contents($this->file_path);
+//        $fileHandlerWriter = fopen(public_path($this->index.'.mp4'), 'a+');
+//        fwrite($fileHandlerWriter, $chunk);
+//        fclose($fileHandlerWriter);
+        try {
+            Log::info(sprintf("Start Uploading From %s To %s With Size %s MB", $this->start, ftell($fileHandler), strlen($chunk) / 1024 / 1024));
+//            Upload-Offset
+            $client = new Client();
+           $nextOffset = $client->patch($this->url, [
+                'headers' => [
+                    'Tus-Resumable'  => '1.0.0',
+                    'Upload-Offset'  => $this->start,
+                    'Content-Length' => strlen($chunk),
+                    'Content-Type'   => 'application/offset+octet-stream',
+                ],
+                'body' => $chunk
+            ]);
+
+
+           Log::error('dddddddddddd');
+            Log::error($nextOffset->getHeaders());
+            Log::error($nextOffset->getBody());
+            Log::error($nextOffset->getStatusCode());
+            Log::error('ssssssssssssssss');
+
+//            if (!$nextOffset)
+//                $this->fail('no offset returned');
+
+        }catch (Exception $exception){
+            Log::error($exception->getMessage());
+        }
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\ReactController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -46,12 +48,51 @@ Route::middleware(['auth'])->group(function (){
     Route::singleton('/account'     , AccountController::class)->except('edit');
 });
 
-Route::get('/test', function (){
+Route::get('/test/{is_new?}', function ($isNew=null){
     $file = base_path('..' . DIRECTORY_SEPARATOR . 'v-1080.mp4');
 
     $chunkSize = 1024 * 1024 *  config('services.vimeo.size', 5); //5MB
 
-    $offsets = range(0, filesize($file), $chunkSize);
+    $fileSize = filesize($file);
+
+    $offsets = range(0, $fileSize, $chunkSize);
+
+    if (!is_null($isNew)){cache()->forget('_url');}
+
+    $url = cache()->remember('_url', now()->addDay(), function () use ($fileSize){
+        return Http::withToken(config('services.vimeo.token'))
+            ->withHeader('Content-Type', 'application/json')
+            ->withHeader('Accept', 'application/vnd.vimeo.*+json;version=3.4')
+            ->post(sprintf("%s/%s", config('services.vimeo.url'), 'me/videos'), [
+                'upload'      => ['approach' => 'tus', 'size' => $fileSize],
+                'name'        => 'Test Hatem ' . now()->format('d-m-Y H:i'),
+                'description' => 'AppTech Task'
+            ])->json('upload.upload_link');
+    });
 
 
+
+    $response = Http::withHeaders([
+        'Tus-Resumable'  => '1.0.0',
+        'Accept'         => 'application/offset+octet-stream',
+    ])->head($url)->headers();
+
+    dd($response);
+    Log::error("Uploading By Tus File With Size = " . $fileSize);
+
+//    $fileHandler = fopen($file, 'rb');
+    foreach ($offsets as $index => $offset){
+//        fseek($fileHandler, $offset);
+//        $chunk = fread($fileHandler, $chunkSize);
+
+//        $path = public_path($index.'.mp4');
+//        $fileHandlerWriter = fopen($path, 'w');
+//        fwrite($fileHandlerWriter, $chunk);
+//        fclose($fileHandlerWriter);
+
+        \App\Jobs\Test::dispatch($url, $file, $offset, $chunkSize)
+//        ->delay(now()->addSeconds(5))
+        ;
+
+    }
 });
